4cb66c10dd56593bb16b6111d59591f30dfc61e7
PetrosiunArtem <petrosiun.av@phystech.edu>
2025-04-13 06:30:42 +0300

    update UserAuditService

--- src/main/java/com/example/app/service/UserAuditService.java
+++ src/main/java/com/example/app/service/UserAuditService.java
@@ -5,7 +5,6 @@ import com.datastax.oss.driver.api.core.cql.BoundStatement;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
-import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.example.app.model.Action;
 import com.example.app.model.UserAudit;
 import lombok.RequiredArgsConstructor;
@@ -22,37 +21,46 @@ import java.util.UUID;
 @Log4j2
 public class UserAuditService {
 
-  @Autowired
-  private CqlSession session;
-
   private static final String INSERT_INTO_USER_AUDIT_QUERY =
       """
       INSERT INTO my_keyspace.user_audit (user_id, event_time, event_type, event_details)
       VALUES (?, ?, ?, ?);
       """;
 
-  private static final String SELECT_ALL_USER_AUDIT_QUERY =
+  private static final String SELECT_USER_AUDIT_QUERY =
       """
       SELECT user_id, event_time, event_type, event_details
       FROM my_keyspace.user_audit
       WHERE user_id = ?;
       """;
+  private static final String SELECT_ALL_USER_AUDIT_QUERY =
+      """
+      SELECT *
+      FROM my_keyspace.user_audit;
+      """;
+  private CqlSession session;
+  private PreparedStatement insertUserAuditStatement;
+  private PreparedStatement selectUserAuditsStatement;
+
+  @Autowired
+  public UserAuditService(CqlSession session) {
+    this.session = session;
+    this.insertUserAuditStatement = session.prepare(INSERT_INTO_USER_AUDIT_QUERY);
+    this.selectUserAuditsStatement = session.prepare(SELECT_USER_AUDIT_QUERY);
+  }
 
   public void insertUserAudit(UUID userId, Action action, String eventDetails) {
     log.info("Inserting user audit for {} with action {}", userId, action);
-    PreparedStatement preparedStatement = session.prepare(INSERT_INTO_USER_AUDIT_QUERY);
     BoundStatement boundStatement =
-        preparedStatement.bind(userId, java.time.Instant.now(), action.toString(), eventDetails);
-
+        insertUserAuditStatement.bind(
+            userId, java.time.Instant.now(), action.toString(), eventDetails);
     session.execute(boundStatement);
   }
 
   public List<UserAudit> readUserAudit(UUID userId) {
     log.info("Reading user audit for {}", userId);
-    PreparedStatement preparedStatement = session.prepare(SELECT_ALL_USER_AUDIT_QUERY);
-    BoundStatement boundStatement = preparedStatement.bind(userId);
+    BoundStatement boundStatement = selectUserAuditsStatement.bind(userId);
     ResultSet result = session.execute(boundStatement);
-
     ArrayList<UserAudit> users = new ArrayList<>();
     for (Row row : result.all()) {
       users.add(
@@ -67,8 +75,7 @@ public class UserAuditService {
 
   List<UserAudit> getAllUsers() {
     log.info("Reading all user audits");
-    SimpleStatement statement = SimpleStatement.newInstance("SELECT * FROM my_keyspace.user_audit");
-    ResultSet result = session.execute(statement);
+    ResultSet result = session.execute(SELECT_ALL_USER_AUDIT_QUERY);
     ArrayList<UserAudit> users = new ArrayList<>();
     for (Row row : result.all()) {
       users.add(
