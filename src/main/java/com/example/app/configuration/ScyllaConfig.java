56d5fbd38886a7ae300634c899b51bcbe4538f92
PetrosiunArtem <petrosiun.av@phystech.edu>
2025-04-21 15:49:12 +0300

    fix ScyllaConfig

--- src/main/java/com/example/app/configuration/ScyllaConfig.java
+++ src/main/java/com/example/app/configuration/ScyllaConfig.java
@@ -18,33 +18,44 @@ public class ScyllaConfig {
   @Value("${scylla.port}")
   private int port;
 
+  @Value("${scylla.keyspace}")
+  private String keyspace;
+
+  @Value("${scylla.datacenter}")
+  private String datacenter;
+
+  @Value("${scylla.host}")
+  private String host;
+
   @Bean
   public CqlSession cqlSession(CqlSessionBuilder sessionBuilder) {
-    InetSocketAddress address = InetSocketAddress.createUnresolved("127.0.0.1", port);
+    InetSocketAddress address = InetSocketAddress.createUnresolved(host, port);
     sessionBuilder = sessionBuilder.addContactPoint(address);
-    sessionBuilder.withKeyspace((CqlIdentifier) null).withLocalDatacenter("datacenter1");
+    sessionBuilder.withKeyspace((CqlIdentifier) null).withLocalDatacenter(datacenter);
 
     try (CqlSession session = sessionBuilder.build()) {
 
       SimpleStatement statement =
-          SchemaBuilder.createKeyspace("my_keyspace")
+          SchemaBuilder.createKeyspace(keyspace)
               .ifNotExists()
-              .withNetworkTopologyStrategy(Map.of("datacenter1", 1))
+              .withNetworkTopologyStrategy(Map.of(datacenter, 1))
               .build();
       session.execute(statement);
       session.execute(
-          """
-            CREATE TABLE IF NOT EXISTS my_keyspace.user_audit (
-                user_id UUID,
-                event_time TIMESTAMP,
-                event_type TEXT,
-                event_details TEXT,
-                PRIMARY KEY (user_id, event_time)
-            ) WITH CLUSTERING ORDER BY (event_time DESC)
-               AND default_time_to_live = 31536000;
-            """);
+          String.format(
+              """
+                   CREATE TABLE IF NOT EXISTS %s.user_audit (
+                       user_id UUID,
+                       event_time TIMESTAMP,
+                       event_type TEXT,
+                       event_details TEXT,
+                       PRIMARY KEY (user_id, event_time)
+                   ) WITH CLUSTERING ORDER BY (event_time DESC)
+                      AND default_time_to_live = 31536000;
+                   """,
+              keyspace));
 
-      return sessionBuilder.withKeyspace("my_keyspace").build();
+      return sessionBuilder.withKeyspace(keyspace).build();
     }
   }
 }
--- src/main/java/com/example/app/service/UserAuditService.java
+++ src/main/java/com/example/app/service/UserAuditService.java
@@ -23,20 +23,20 @@ public class UserAuditService {
 
   private static final String INSERT_INTO_USER_AUDIT_QUERY =
       """
-      INSERT INTO my_keyspace.user_audit (user_id, event_time, event_type, event_details)
+      INSERT INTO user_audit (user_id, event_time, event_type, event_details)
       VALUES (?, ?, ?, ?);
       """;
 
   private static final String SELECT_USER_AUDIT_QUERY =
       """
       SELECT user_id, event_time, event_type, event_details
-      FROM my_keyspace.user_audit
+      FROM user_audit
       WHERE user_id = ?;
       """;
   private static final String SELECT_ALL_USER_AUDIT_QUERY =
       """
       SELECT *
-      FROM my_keyspace.user_audit;
+      FROM user_audit;
       """;
   private CqlSession session;
   private PreparedStatement insertUserAuditStatement;
@@ -50,7 +50,7 @@ public class UserAuditService {
   }
 
   public void insertUserAudit(UUID userId, Action action, String eventDetails) {
-    log.info("Inserting user audit for {} with action {}", userId, action);
+    log.debug("Inserting user audit for {} with action {}", userId, action);
     BoundStatement boundStatement =
         insertUserAuditStatement.bind(
             userId, java.time.Instant.now(), action.toString(), eventDetails);
@@ -58,7 +58,7 @@ public class UserAuditService {
   }
 
   public List<UserAudit> readUserAudit(UUID userId) {
-    log.info("Reading user audit for {}", userId);
+    log.debug("Reading user audit for {}", userId);
     BoundStatement boundStatement = selectUserAuditsStatement.bind(userId);
     ResultSet result = session.execute(boundStatement);
     ArrayList<UserAudit> users = new ArrayList<>();
@@ -74,7 +74,7 @@ public class UserAuditService {
   }
 
   List<UserAudit> getAllUsers() {
-    log.info("Reading all user audits");
+    log.debug("Reading all user audits");
     ResultSet result = session.execute(SELECT_ALL_USER_AUDIT_QUERY);
     ArrayList<UserAudit> users = new ArrayList<>();
     for (Row row : result.all()) {
